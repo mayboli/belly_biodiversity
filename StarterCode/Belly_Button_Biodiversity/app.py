6c6346f90070051eafcc64795d57616b03daeffb
Maybo <maybo528@gmail.com>
2018-10-01 20:25:37 -0700

    update2

--- StarterCode/Belly_Button_Biodiversity/app.py
+++ StarterCode/Belly_Button_Biodiversity/app.py
@@ -12,7 +12,7 @@ from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #################################################
 # Database Setup
